2b97cdef6e2153ac2f7a4ca3ef016d31ed73587b
Arafullah Shihab <shihabaraf@gmail.com>
2018-09-09 00:32:55 -0400

    Added redis for caching blogs

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://mongoAraf:coinJar1!@ds249942.mlab.com:49942/blog_dev?authMechanism=SCRAM-SHA-1',
   cookieKey: '123123123'
 };
